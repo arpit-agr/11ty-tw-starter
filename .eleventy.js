0735cbfc23fa9a1ab04b96695919d224df81439f
arpit-agr <arpit.agrawal2502@gmail.com>
2023-08-09 16:14:12 +0530

    generate css custom props from design tokens and use tailwind as a utility generator

--- .eleventy.js
+++ .eleventy.js
@@ -1,55 +1,47 @@
 const directoryOutputPlugin = require("@11ty/eleventy-plugin-directory-output");
 const htmlmin = require("html-minifier");
-const CleanCSS = require("clean-css");
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.setQuietMode(true);
-  eleventyConfig.addPlugin(directoryOutputPlugin);
+module.exports = function (config) {
+	config.setQuietMode(true);
+	config.addPlugin(directoryOutputPlugin);
 
-  //Passthrough copy
-  // eleventyConfig.addPassthroughCopy("./src/fonts");
-	// eleventyConfig.addPassthroughCopy("./src/images");
-	// eleventyConfig.addPassthroughCopy("./src/scripts");
-  // eleventyConfig.addPassthroughCopy({"./src/favicons": "/"});
-	// eleventyConfig.addPassthroughCopy("./src/manifest.webmanifest");
+	//Passthrough copy
+	// config.addPassthroughCopy("./src/fonts");
+	// config.addPassthroughCopy("./src/images");
+	// config.addPassthroughCopy("./src/scripts");
+	// config.addPassthroughCopy({"./src/favicons": "/"});
+	// config.addPassthroughCopy("./src/manifest.webmanifest");
 
-  //Watch target
-	// eleventyConfig.addWatchTarget("./src/_includes/css/");
-	// eleventyConfig.addWatchTarget('./src/scripts/');
+	//Watch target
+	config.addWatchTarget("./src/css/");
 
-  //Filter
-  eleventyConfig.addFilter("cssmin", function(code) {
-    if(process.env.NODE_ENV === "production") {
-      return new CleanCSS({}).minify(code).styles;
-    }
-    else {
-      return code
-    }
-  });
-
-  //Transforms
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-    if(process.env.NODE_ENV === "production" && this.outputPath && this.outputPath.endsWith(".html") ) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: false,
-        removeComments: true,
-        collapseWhitespace: true,
-        collapseBooleanAttributes: true
-      });
-      return minified;
-    }
-
-    return content;
-  });
-
-  return {
-    // dataTemplateEngine: "njk",
-		markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-		dir: {
-			input: 'src',
-			output: 'public'
+	//Transforms
+	config.addTransform("htmlmin", function (content, outputPath) {
+		// Eleventy 1.0+: use this.inputPath and this.outputPath instead
+		if (
+			process.env.NODE_ENV === "production" &&
+			this.outputPath &&
+			this.outputPath.endsWith(".html")
+		) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: false,
+				removeComments: true,
+				collapseWhitespace: true,
+				collapseBooleanAttributes: true,
+			});
+			return minified;
 		}
+
+		return content;
+	});
+
+	return {
+		dataTemplateEngine: "njk",
+		markdownTemplateEngine: "njk",
+		htmlTemplateEngine: "njk",
+		dir: {
+			input: "src",
+			output: "public",
+		},
 	};
 };
